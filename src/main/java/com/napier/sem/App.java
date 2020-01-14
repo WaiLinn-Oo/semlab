aabcc3a00e8710bcb396d601b373f3b896f34c1e
WaiLinn-Oo <wailinnoofirst@gmail.com>
2020-01-14 21:30:58 +0630

    Mongo DB updated

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,8 +9,11 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Connect to MongoDB on local system - we're using port 27000 //
+        // MongoClient mongoClient = new MongoClient("localhost", 27000); //
+
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -18,7 +21,7 @@ public class App
         // Create a document to store
         Document doc = new Document("name", "Kevin Chalmers")
                 .append("class", "Software Engineering Methods")
-                .append("year", "2018/19")
+                .append("year", "2019/20")
                 .append("result", new Document("CW", 95).append("EX", 85));
         // Add document to collection
         collection.insertOne(doc);
